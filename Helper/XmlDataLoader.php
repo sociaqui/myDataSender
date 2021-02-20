5f26642d459618c38b004d8df4bcf7dcfc58569d
Jarek <sociaqui@gmail.com>
2021-02-21 00:56:09 +0100

    added xml stub for future expansion

---
+++ Helper/XmlDataLoader.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Sociaqui\DataSenderBundle\Helper;
+
+
+class XmlDataLoader implements DataLoader
+{
+    /**
+     * @inheritDoc
+     */
+    public static final function loadDataFromFile($name){
+        //TODO: implement the actual logic
+    }
+}
